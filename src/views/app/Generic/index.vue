51160dbec1d377c49bb1d6d2f7526480fe280fd8
dzoukou <dzoukouraoul@gmail.com>
2022-06-28 14:25:47 +0100

    WP 1 - Fix table navigation

--- src/views/app/Generic/index.vue
+++ src/views/app/Generic/index.vue
@@ -1,35 +1,28 @@
 <template>
-  <div :key="table">
+  <div>
     <b-card body-class="p-0">
       <table-pagination :search.sync="search" :per-page.sync="perPage" :current-page.sync="currentPage"
                         :on-new-element="()=> $refs.modal.openModal(true, {})" :total-rows="totalRows"
                         :on-delete-elements="()=> $refs.table.deleteSelected()"/>
     </b-card>
     <b-card>
-      <Datatable ref="table" link="table-user-edit" :search="search" :entity="table" :default-sort-field="definition.defaultSortField"
-                 :per-page="perPage" :current-page="currentPage" :total-rows.sync="totalRows"
-                 :fields="definition.fields" :primary-key-column="definition.primaryKey"/>
+      <Datatable :key="table" ref="table" link="table-user-edit" :search="search" :entity="table"
+                 :default-sort-field="definition.defaultSortField" :per-page="perPage" :current-page="currentPage"
+                 :total-rows.sync="totalRows" :fields="definition.fields" :primary-key-column="definition.primaryKey"/>
     </b-card>
-    <generic-modal @reload-table="$refs.table.reload()" :table="table" :definition="definition" :table-definition-key="table" :title="'create '+table" ref="modal"/>
+    <generic-modal @reload-table="$refs.table.reload()" :table="table" :definition="definition"
+                   :table-definition-key="table" :title="'create '+table" ref="modal"/>
   </div>
 </template>
 
 <script>
 
 import {
-  BFormGroup,
-  BFormSelect,
-  BModal,
-  BForm,
-  BRow,
-  BCol,
-  BFormInput,
   BCard,
 } from 'bootstrap-vue'
 import TablePagination from '@/layouts/components/TablePagination.vue'
 import GenericModal from '@/views/app/Generic/modal';
 import Tables from '../../../table'
-import { mapState } from "vuex";
 
 const Datatable = () => import('@/layouts/components/DataTables.vue')
 
@@ -38,14 +31,7 @@ export default {
     GenericModal,
     TablePagination,
     Datatable,
-    BFormGroup,
     BCard,
-    BFormSelect,
-    BModal,
-    BForm,
-    BRow,
-    BCol,
-    BFormInput,
   },
   data() {
     return {
@@ -61,6 +47,14 @@ export default {
     },
     definition() {
       return Tables[this.table]
+    },
+  },
+  watch: {
+    table() {
+      this.search = ''
+      // this.perPage = 10
+      this.currentPage = 1
+      this.totalRows = 0
     },
   },
   methods: {},
