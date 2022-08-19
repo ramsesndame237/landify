8339ee7e0343f313fae1d2f90b35a13953319074
fredchess97 <fredericktabi30@gmail.com>
2022-08-19 11:12:03 +0100

    wip REview WP2 and WP3

--- src/table/review.js
+++ src/table/review.js
@@ -7,10 +7,15 @@ export default {
 contactperson_customergroup_grp doesnt send all values <br>
 Do we have to keep grouptickettab ?<br>
 user_customergroup_grp doesnt send usertype_name and team_name`,
+  company_relations: `frontend_2_1_3_2 doesnt send cotradiction_payment_received <br>`,
+  contactperson: `Need entity for list. contactperson doesnt return city_name, contactdetails infos and userId`,
   // endregion
   // region Work Package 3
   area: `
 Create: Impossible to associate usage_type to area, usagetype  from contains date fields`,
+  area_relations: `Seem like frontend_3_2_3_1 doent return all relative contract related to an area<br>
+frontend_3_2_3_1 doesnt return usagetype_id<br>
+`,
   recurringpayment: `Impossible to create a recurring payment, we have an internal server error`,
   criteria_relations: `Impossible to get the criteria contract relation, we have an internal server error<br>
 Contract: Missing Unit on the contract relation`,
