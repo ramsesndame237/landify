4b342742702322a25e0967f684c5aec85deb4127
fredchess97 <fredericktabi30@gmail.com>
2022-09-02 18:20:15 +0100

    wip WP6 interfaces

---
+++ src/views/app/CustomComponents/WP6/OperationRecapCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="">
+    <h5 class="mb-1 mt-2 font-weight-bolder">Yesterday, May 2</h5>
+    <b-card>
+      <b-card-body class="d-flex justify-content-between align-items-center">
+        <div class="">
+          <h4 class="font-weight-bolder" style="color: black">Widerspruchsfrist</h4>
+          <h5 style="color: #ccc">Some descriptions about this</h5>
+          <div class="">11:00 PM — 4:00 AM</div>
+        </div>
+        <div class="d-flex flex-column justify-content-center align-items-center">
+          <div class="d-flex justify-content-center align-items-center" style="height: 30px; width: 30px; background-color: #D51130; border-radius: 100%">
+            <b-icon-bag-fill color="#fff"/>
+          </div>
+          <b-icon-caret-down/>
+        </div>
+      </b-card-body>
+    </b-card>
+  </div>
+</template>
+
+<script>
+import { BRow, BCol, BCard, BCardBody, BIconBagFill, BIconCaretDown } from 'bootstrap-vue'
+export default {
+  name: 'OperationRecapCard',
+  components: {
+    BRow,
+    BCard,
+    BCardBody,
+    BCol,
+    BIconBagFill,
+    BIconCaretDown,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/app/Page/OperationsPage.vue
+++ src/views/app/Page/OperationsPage.vue
@@ -1,6 +1,6 @@
 <template>
   <b-row>
-    <b-col lg="9">
+    <b-col lg="8">
       <b-card>
         <b-card-body>
           <div class="d-flex justify-content-between align-items-center">
@@ -41,14 +41,54 @@
       <validation-card title="Fililalleiter V" subtitle="Emeut an diese Aufgabe erinnert werden"/>
       <validation-card title="Fililalleiter V" subtitle="Emeut an diese Aufgabe erinnert werden"/>
     </b-col>
-    <b-col lg="3"></b-col>
+    <b-col lg="4">
+      <div class="d-flex justify-content-between align-items-center">
+        <h4 class="font-weight-bolder">Upcoming Events</h4>
+        <b-dropdown
+            variant="outline-danger"
+        >
+          <template #button-content>
+            <b-icon-calendar-week class="mr-1"/>
+            <span>Calendar</span>
+<!--            <b-icon-caret-down/>-->
+          </template>
+          <b-dropdown-item>option 1</b-dropdown-item>
+          <b-dropdown-item>option 2</b-dropdown-item>
+          <b-dropdown-item>option 3</b-dropdown-item>
+        </b-dropdown>
+      </div>
+      <operation-recap-card/>
+      <operation-recap-card/>
+      <operation-recap-card/>
+      <b-row>
+        <b-col lg="6">
+          <b-card>
+            <b-card-body>
+              <h6 style="color: #ccc">abcbcaoasjsijs</h6>
+              <h4 class="font-weight-bolder mb-2" style="color: black">Title</h4>
+              <b-button class="m-auto" variant="danger">Open</b-button>
+            </b-card-body>
+          </b-card>
+        </b-col>
+        <b-col lg="6">
+          <b-card>
+            <b-card-body>
+              <h6 style="color: #ccc">abcbcaoasjsijs</h6>
+              <h4 class="font-weight-bolder mb-2" style="color: black">Title</h4>
+              <b-button class="m-auto" variant="danger">Open</b-button>
+            </b-card-body>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-col>
   </b-row>
 </template>
 
 <script>
-import { BButton, BCol, BRow, BCard, BCardBody, BIconAlarm } from 'bootstrap-vue'
+import { BButton, BCol, BRow, BCard, BCardBody, BIconAlarm, BDropdown, BDropdownItem, BIconCalendarWeek, BIconCaretDown } from 'bootstrap-vue'
 import AdditionalCostsCard from '@/views/app/CustomComponents/WP6/AdditionalCostsCard'
 import ValidationCard from '@/views/app/CustomComponents/WP6/ValidationCard'
+import OperationRecapCard from '@/views/app/CustomComponents/WP6/OperationRecapCard'
 
 export default {
   name: 'OperationsPage',
@@ -61,6 +101,11 @@ export default {
     BIconAlarm,
     AdditionalCostsCard,
     ValidationCard,
+    BDropdown,
+    BDropdownItem,
+    BIconCalendarWeek,
+    BIconCaretDown,
+    OperationRecapCard,
   },
 }
 </script>
