cea8a0d51213c2bcf2bb85337b57646afd74cac1
dzoukou <dzoukouraoul@gmail.com>
2022-11-08 09:35:23 +0100

    commit

--- src/views/app/Kanban/index.vue
+++ src/views/app/Kanban/index.vue
@@ -2,11 +2,11 @@
   <div>
     <kanban-board :blocks="blocks" :stages="stages">
       <div v-for="block in blocks" :slot="block.id" :key="block.id" class="item">
-        <!--        <simple-block :title="block.title" :description="block.description" v-if="block.id & 1"></simple-block>-->
         <summary-block title="Widerspruchsfrist" sub_title="Filiale 1" hours="54" percents="50" documents_nb="14"
                        left_days="14" participants_nb="17" :with-attach="block.id & 1 ? true : false"/>
       </div>
-      <div v-for="(stage,idx) in stages" :slot="'footer-'+stage" :key="stage" style="padding: 10px">
+      <div v-for="(stage,idx) in stages" :slot="'footer-'+stage" :key="stage" :id="'button-'+stage"
+           style="padding: 10px">
         <b-button variant="primary" block @click="createTicket(columns[idx])">
           New Ticket
         </b-button>
@@ -109,3 +109,190 @@ export default {
   },
 }
 </script>
+
+
+<style lang="scss">
+/*@import '~vue-kanban/src/assets/kanban.scss';*/
+$ease-out: all .3s cubic-bezier(0.23, 1, 0.32, 1);
+$on-hold: #FB7D44;
+$in-progress: #2A92BF;
+$needs-review: #F4CE46;
+$approved: #00B961;
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.drag-container {
+  //max-width: 1000px;
+  margin: 20px auto;
+}
+
+.drag-list {
+  display: flex;
+  align-items: flex-start;
+
+  @media (max-width: 690px) {
+    display: block;
+  }
+}
+
+.drag-column {
+  flex: 1;
+  margin: 0 10px;
+  position: relative;
+  background: #E9E9E9;
+  overflow: hidden;
+
+  @media (max-width: 690px) {
+    margin-bottom: 30px;
+  }
+
+  h2 {
+    font-size: 0.8rem;
+    margin: 0;
+    text-transform: uppercase;
+    font-weight: 600;
+  }
+
+  &-on-hold {
+    .drag-column-header,
+    .is-moved,
+    .drag-options {
+      background: $on-hold;
+    }
+  }
+
+  &-in-progress {
+    .drag-column-header,
+    .is-moved,
+    .drag-options {
+      background: $in-progress;
+    }
+  }
+
+  &-needs-review {
+    .drag-column-header,
+    .is-moved,
+    .drag-options {
+      background: $needs-review;
+    }
+  }
+
+  &-approved {
+    .drag-column-header,
+    .is-moved,
+    .drag-options {
+      background: $approved;
+    }
+  }
+}
+
+.drag-column-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+}
+
+.drag-inner-list {
+  min-height: 50px;
+}
+
+.drag-item {
+  margin: 10px;
+  background: rgba(white, 1);
+  //transition: $ease-out;
+
+  &.is-moving {
+    transform: scale(1.1);
+    background: rgba(white, 0.8);
+  }
+
+}
+
+.drag-header-more {
+  cursor: pointer;
+}
+
+.drag-options {
+  position: absolute;
+  top: 44px;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  transform: translateX(100%);
+  opacity: 0;
+  transition: $ease-out;
+
+  &.active {
+    transform: translateX(0);
+    opacity: 1;
+  }
+
+  &-label {
+    display: block;
+    margin: 0 0 5px 0;
+
+    input {
+      opacity: 0.6;
+    }
+
+    span {
+      display: inline-block;
+      font-size: 0.9rem;
+      font-weight: 400;
+      margin-left: 5px;
+    }
+  }
+}
+
+/* Dragula CSS  */
+
+.gu-mirror {
+  position: fixed !important;
+  margin: 0 !important;
+  z-index: 9999 !important;
+  opacity: 0.8;
+  list-style-type: none;
+}
+
+.gu-hide {
+  display: none !important;
+}
+
+.gu-unselectable {
+  -webkit-user-select: none !important;
+  -moz-user-select: none !important;
+  -ms-user-select: none !important;
+  user-select: none !important;
+}
+
+.gu-transit {
+  opacity: 0.2;
+}
+
+/* Demo info */
+
+.section {
+  padding: 20px;
+  text-align: center;
+
+  a {
+    color: white;
+    text-decoration: none;
+    font-weight: 300;
+  }
+
+  h4 {
+    font-weight: 400;
+
+    a {
+      font-weight: 600;
+    }
+  }
+}
+</style>
