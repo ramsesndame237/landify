714df16f74b131bdcc1117f78026d0bb04fe9e97
dzoukou <dzoukouraoul@gmail.com>
2023-04-25 12:54:37 +0100

    Import page, new api design

--- src/views/app/Page/ImportPage.vue
+++ src/views/app/Page/ImportPage.vue
@@ -1,53 +1,86 @@
 <template>
   <div class="">
     <!--UPLOAD-->
-    <form enctype="multipart/form-data" novalidate>
+    <form enctype="multipart/form-data" novalidate v-if="!result">
       <h1>Upload A File</h1>
       <div class="dropbox">
-        <input type="file" :name="uploadFieldName" :disabled="processing"
+        <input type="file" :name="uploadFieldName" :disabled="loading"
                accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
                class="input-file" @change="fileCount = $event.target.files.length; file = $event.target.files[0]">
-        <p v-if="!file">
-          Drag your file here to begin<br> or click to browse </p>
-        <p v-if="file">
-          File charged </p>
-        <p v-if="processing" class="loader">
-          <b-icon-arrow-repeat/>
-        </p>
-        <div v-if="success" class="loader">
-          <b-icon-check/>
-          <h4>done</h4>
-        </div>
+        <p v-if="!file">Drag your file here to begin<br> or click to browse </p>
+        <p v-if="file">File charged </p>
       </div>
+      <b-form-group class="mt-2" label="Tables to import">
+        <b-form-radio-group v-model="table" :options="entities.map(entity => ({text: titles[entity], value: entity}))"
+                            name="radio-inline"/>
+      </b-form-group>
       <div class="mt-2">
-        <b-button variant="danger" @click="upload(file)">Import</b-button>
+        <b-button variant="danger" :disabled="loading" @click="upload">
+          Check File
+          <b-spinner v-if="loading" small/>
+        </b-button>
       </div>
     </form>
-    <div class="mt-5">
+    <div v-else>
       <b-card>
         <b-tabs ref="tabs" pills>
-          <b-tab v-for="(entity, index) in entities" :key="index" :title="entity" lazy>
-            <table v-if="result[entity]" class="mt-2 mt-xl-0 w-100">
+          <b-tab v-for="(entity, index) in entities.filter(e => !!result[e])" :key="index" :title="titles[entity]" lazy>
+            <table v-if="result[entity]" class="my-2 table w-100">
+              <thead>
               <tr>
                 <th class="pb-50 font-weight-bold">Total</th>
-                <td class="pb-50">{{ getCount(entity) }}</td>
+                <th class="pb-50">{{ getCount(entity) }}</th>
                 <th class="pb-50 font-weight-bold">Inserted</th>
-                <td class="pb-50">{{ getCount(entity, 'added') }}</td>
+                <th class="pb-50">{{ getCount(entity, 'added') }}</th>
                 <th class="pb-50 font-weight-bold">Updated</th>
-                <td class="pb-50">{{ getCount(entity, 'updated') }}</td>
+                <th class="pb-50">{{ getCount(entity, 'updated') }}</th>
                 <th class="pb-50 font-weight-bold">Unchanged</th>
-                <td class="pb-50">{{ getCount(entity, 'unchanged') }}</td>
+                <th class="pb-50">{{ getCount(entity, 'unchanged') }}</th>
                 <th class="pb-50 font-weight-bold">Failed</th>
-                <td class="pb-50">{{ getCount(entity, 'failed') }}</td>
+                <th class="pb-50">{{ getCount(entity, 'failed') }}</th>
               </tr>
+              </thead>
             </table>
-            <data-tables :entity="entity" :selectable="false" :with-actions="false" :fields="fields"
-                         :items="getResult(entity)"/>
+            <b-table-simple class="table-responsive">
+              <b-thead>
+                <b-tr>
+                  <b-th>
+                    <b-form-checkbox v-model="selected[entity]" :disabled="disabled" @change="onSelectChange(entity)"/>
+                  </b-th>
+                  <b-th>Line</b-th>
+                  <b-th v-for="(field,i) in fields" :key="i">{{ $t('attribute.' + field.key) }}</b-th>
+                  <!--                  <b-th class="text-center">-->
+                  <!--                    Action-->
+                  <!--                  </b-th>-->
+                </b-tr>
+              </b-thead>
+              <b-tbody>
+                <b-tr v-if="getResult(entity).length===0">
+                  <b-td :colspan="3+fields.length" class="text-center">No Data available</b-td>
+                </b-tr>
+                <b-tr v-for="(row, i) in getResult(entity)" :key="i">
+                  <b-td>
+                    <b-form-checkbox v-model="row.__selected" :disabled="disabled"/>
+                  </b-td>
+                  <b-td>{{ row.line }}</b-td>
+                  <template v-for="(column,i) in fields">
+                    <b-td v-if="row[column.key]" :key="i"
+                          :title="row[column.key].old_value ? `Old Value: ${row[column.key].old_value}`: null"
+                          :style="{background: row[column.key].color}">
+                      {{ row[column.key].new_value }}
+                    </b-td>
+                    <b-td v-else :key="i"/>
+                  </template>
+
+                </b-tr>
+              </b-tbody>
+            </b-table-simple>
           </b-tab>
           <template #tabs-end>
-            <div>
-              <label for="">Status</label>
-              <b-form-select v-model="status" :options="status"/>
+            <div class=" d-flex align-items-center ml-auto">
+              <b-button variant="primary">Import All</b-button>
+              <label class="mx-1">Status</label>
+              <b-form-select v-model="status" :options="statusList" style="width: 100px"/>
             </div>
           </template>
         </b-tabs>
@@ -77,18 +110,28 @@ export default {
   },
   data() {
     return {
-      processing: false,
+      loading: false,
       success: false,
       uploadFieldName: 'file',
       fileCount: 0,
       file: null,
       errors: [],
       errorsCnt: 0,
+      disabled: false,
+      table: 'partner_companies',
       entities: [
         'partner_companies', 'companies', 'contact_persons', 'locations', 'pos', 'areas'
       ],
+      titles: {
+        'partner_companies': 'Partner Companies',
+        'companies': 'Companies',
+        'contact_persons': 'Contact persons',
+        'locations': 'Locations',
+        'pos': 'Point of sales',
+        'areas': 'Areas'
+      },
+      selected: {},
       fields: [
-        { key: 'line' },
         { key: 'status' },
         { key: 'partnercompany_name' },
         { key: 'partnercompany_shortname' },
@@ -107,8 +150,14 @@ export default {
         // { key: 'contactdetails_commercialregisterno' },
         // { key: 'contactdetails_website' },
       ],
-      result: {},
-      statusList: ['added', 'updated', 'unchanged', 'failed'],
+      result: null,
+      statusList: [
+        { text: 'All', value: '' },
+        { text: 'Added', value: 'added' },
+        { text: 'Updated', value: 'updated' },
+        { text: 'Unchanged', value: 'unchanged' },
+        { text: 'Failed', value: 'failed' }
+      ],
       status: '',
 
     }
@@ -118,19 +167,19 @@ export default {
     this.reset()
   },
   methods: {
+    onSelectChange(entity) {
+      this.result[entity].forEach(row => {
+        row.__selected = !!this.selected[entity]
+      })
+    },
     getResult(entity) {
-      if (!this.result[entity]) return []
-      return [
-        ...this.result[entity].added.map(e => ({ ...this.getEntity(e), status: 'added' })),
-        ...this.result[entity].updated.map(e => ({ ...this.getEntity(e), status: 'updated' })),
-        ...this.result[entity].unchanged.map(e => ({ ...this.getEntity(e), status: 'unchanged' })),
-        ...this.result[entity].failed.map(e => ({ ...this.getEntity(e), status: 'failed' })),
-      ]
+      if (!this.status) return this.result[entity]
+      return this.result[entity].filter(row => row.status === status)
     },
     getEntity(e) {
       return {
         ...e,
-        ...e.colunms.reduce((obj, c) => {
+        ...e.columns.reduce((obj, c) => {
           obj[c.name] = c.new_value
           return obj
         }, {})
@@ -138,8 +187,8 @@ export default {
     },
     getCount(entity, status) {
       const results = this.result[entity]
-      if (!status) return results.added.length + results.updated.length + results.unchanged.length + results.failed.length
-      return results[status].length
+      if (!status) return results.length
+      return results.filter(row => row.status === status).length
     },
     reset() {
       // reset form to initial state
@@ -147,15 +196,35 @@ export default {
       this.uploadedFiles = []
       this.uploadError = null
     },
-    async upload(file) {
-      if (!file) return this.$errorToast('Please insert a file')
+    async upload() {
+      if (!this.file) return this.$errorToast('Please insert a file')
+      if (!this.table) return this.$errorToast('Please select a table')
       const formData = new FormData
-      formData.append('file', file)
+      formData.append('file', this.file)
       formData.append('leaves', ['Partner Company'])
-      this.processing = true
-      this.$http.post('provisionings/partnercompany/checking', formData, { headers: { 'content-type': 'form-data' } })
+      this.loading = true
+      this.$http.post('/provisionings/partnercompany/checking', formData, { headers: { 'content-type': 'form-data' } })
         .then(({ data }) => {
           console.log(data)
+          this.entities.forEach(function (entity) {
+            if (!data.data[entity]) return
+            data.data[entity].forEach(row => {
+              row.__selected = false
+              row.columns.forEach(column => {
+                row[column.name] = column
+                switch (column.action) {
+                  case 'added':
+                    column.color = 'greenyellow'
+                    break
+                  case 'changed':
+                    column.color = 'yellow'
+                    break
+                  case 'failed':
+                    column.color = 'red'
+                }
+              })
+            })
+          })
           this.result = data.data
           this.success = true
         })
@@ -163,7 +232,7 @@ export default {
           this.$errorToast('Server Error')
         })
         .finally(() => {
-          this.processing = false
+          this.loading = false
         })
     }
   },
@@ -177,7 +246,7 @@ export default {
   background: lightcyan;
   color: dimgray;
   padding: 10px 10px;
-  min-height: 200px; /* minimum height */
+  min-height: 100px; /* minimum height */
   position: relative;
   cursor: pointer;
 }
@@ -185,7 +254,7 @@ export default {
 .input-file {
   opacity: 0; /* invisible but it's there! */
   width: 100%;
-  height: 200px;
+  height: 100px;
   position: absolute;
   cursor: pointer;
 }
@@ -205,30 +274,5 @@ export default {
   font-weight: 700;
   text-align: center;
   //padding: 70px 0;
-}
-
-.bi-arrow-repeat {
-  -animation: spin .7s infinite linear;
-  -webkit-animation: spin2 .7s infinite linear;
-}
-
-@-webkit-keyframes spin2 {
-  from {
-    -webkit-transform: rotate(0deg);
-  }
-
-  to {
-    -webkit-transform: rotate(360deg);
-  }
-}
-
-@keyframes spin {
-  from {
-    transform: scale(1) rotate(0deg);
-  }
-
-  to {
-    transform: scale(1) rotate(360deg);
-  }
 }
 </style>
