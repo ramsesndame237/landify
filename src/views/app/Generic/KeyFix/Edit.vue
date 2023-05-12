c58a490ef294734b9a83c5c7afbbc27fb1c5c125
dzoukou <dzoukouraoul@gmail.com>
2023-05-12 08:06:55 +0100

    commit

--- src/views/app/Generic/KeyFix/Edit.vue
+++ src/views/app/Generic/KeyFix/Edit.vue
@@ -1,85 +1,82 @@
 <template>
-  <component :is="definition.customPage || 'edit'" ref="edit" :key="$route.path" />
+  <component :is="definition.customPage || 'edit'" ref="edit" :key="$route.path"/>
 </template>
 
 <script>
-import table from "@/table/index";
-import Edit from "../Edit.vue";
-import Table from "@/table";
+import table from '@/table/index'
+import Edit from '../Edit.vue'
+import Table from '@/table';
 
 export default {
-  watch: {},
-  name: "EditPage",
+  name: 'EditPage',
   components: { Edit },
   computed: {
     definition() {
-      return table[this.$route.params.table];
+      return table[this.$route.params.table]
     },
   },
   methods: {
     async handleRouteChange(next) {
-      console.log("before route leave", this);
-      if (Object.hasOwnProperty.call(this.$refs.edit.$refs.form, "hasChanges")) {
-        if (!this.$refs.edit.view && this.$refs.edit.$refs.form.hasChanges()) {
-          const result = await this.$swal({
-            title: "Are you sure you want to leave?",
-            text: "Your changes will be loosed",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonText: "Yes",
-            customClass: {
-              confirmButton: "btn btn-primary",
-              cancelButton: "btn btn-outline-danger ml-1",
-            },
-            buttonsStyling: false,
-          });
-          if (result.value) return next();
-        }
+      console.log('before route leave', this)
+      if (!this.$refs.edit.view && this.$refs.edit.$refs.form.hasChanges()) {
+        const result = await this.$swal({
+          title: 'Are you sure you want to leave?',
+          text: 'Your changes will be loosed',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonText: 'Yes',
+          customClass: {
+            confirmButton: 'btn btn-primary',
+            cancelButton: 'btn btn-outline-danger ml-1',
+          },
+          buttonsStyling: false,
+        })
+        if (result.value) next()
       } else {
-        return next();
+        next()
       }
     },
   },
   beforeRouteEnter(to, from, next) {
-    to.meta.pageTitle =
-      Table[to.params.table]?.title || `headline~${to.params.table}~list`;
+    to.meta.pageTitle = Table[to.params.table]?.title || `headline~${to.params.table}~list`
     to.meta.breadcrumb = [
       {
         text: to.params.table,
-        to: { name: "table", params: { table: to.params.table } },
+        to: { name: 'table', params: { table: to.params.table } },
         active: false,
       },
       {
-        text: "Details",
+        text: 'Details',
         active: true,
       },
-    ];
-    next();
+    ]
+    next()
   },
   beforeRouteUpdate(to, from, next) {
     // if it is tab update
-    if (to.params.id === from.params.id) return next();
+    if (to.params.id === from.params.id) return next()
     this.handleRouteChange(() => {
-      to.meta.pageTitle =
-        Table[to.params.table]?.title || `headline~${to.params.table}~list`;
+      to.meta.pageTitle = Table[to.params.table]?.title || `headline~${to.params.table}~list`
       to.meta.breadcrumb = [
         {
           text: to.params.table,
-          to: { name: "table", params: { table: to.params.table } },
+          to: { name: 'table', params: { table: to.params.table } },
           active: false,
         },
         {
-          text: "Details",
+          text: 'Details',
           active: true,
         },
-      ];
-      next();
-    });
+      ]
+      next()
+    })
   },
   beforeRouteLeave(to, from, next) {
-    this.handleRouteChange(next);
+    this.handleRouteChange(next)
   },
-};
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
