b80645b7db22d78e0d9dc3c85f3863554dc85278
ivan nzima <nzimaivan007@gmail.com>
2023-05-15 18:14:26 +0100

    update city form style

--- src/views/app/FormComponent/CityForm.vue
+++ src/views/app/FormComponent/CityForm.vue
@@ -1,22 +1,19 @@
 <template>
   <b-row>
     <b-col cols="12" >
-      <b-row>
-        <b-col cols="4">
-          <span>{{$t('attribute.city_zip')}}/{{$t('attribute.city_name')}}</span>
-        </b-col>
-        <b-col cols="2">
-          <field :inline="false" :noLabel="true" ref="fields" :disabled="disabled" :entity="entity" :table-definition="tableDefinition" :field="getField('city_zip')"/>
-        </b-col>
-        <b-col cols="6">
+      <b-form-group :label="$t('attribute.city_zip') + '/' + $t('attribute.city_name')"
+        label-cols-md="4">
+        <div class="d-flex">
+          <field style="flex-grow: 1; margin-right: 20px;" :inline="false" :noLabel="true" ref="fields" :disabled="disabled" :entity="entity" :table-definition="tableDefinition" :field="getField('city_zip')"/>
           <field :inline="false" :noLabel="true" ref="fields" :disabled="disabled" :entity="entity" :table-definition="tableDefinition" :field="getField('city_name')"/>
-        </b-col>
-      </b-row>
+
+        </div>
+      </b-form-group>
     </b-col>
     <b-col cols="12">
       <field :inline="true" ref="fields" :disabled="disabled" :entity="entity" :table-definition="tableDefinition" :field="getField('state')"/>
     </b-col>
-  
+
     <b-col cols="12">
       <field :inline="true" ref="fields" :disabled="disabled" :entity="entity" :table-definition="tableDefinition" :field="getField('country_id')"/>
     </b-col>
