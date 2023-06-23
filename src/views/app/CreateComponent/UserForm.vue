b35ef974b30bcd6302df10a5015ee72cd58a024f
ivan nzima <nzimaivan007@gmail.com>
2023-06-23 15:48:09 +0100

    fix issue on view user info

--- src/views/app/CreateComponent/UserForm.vue
+++ src/views/app/CreateComponent/UserForm.vue
@@ -3,7 +3,7 @@
     <b-form @submit.prevent="passes(submit)" autocomplete="off">
       <b-row>
         <b-col v-for="(field,index) in formFields" :key="index" cols="12" :md="cols">
-          <field v-if="field" ref="fields" :disabled="loading || field.disabled || (!create && field.disableOnUpdate)"
+          <field v-if="field" ref="fields" :disabled="loading || disabled || field.disabled || (!create && field.disableOnUpdate)"
                  :create="create" :inline="inline" :entity="entity" :table-definition="tableDefinition" :field="field"/>
         </b-col>
       </b-row>
