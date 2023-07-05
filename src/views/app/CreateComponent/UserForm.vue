1ce011f23f9fc1c41adfd8250d85b8fe4b00e8f9
ivan nzima <nzimaivan007@gmail.com>
2023-07-05 11:09:27 +0100

    give up the city_id select field issue

--- src/views/app/CreateComponent/UserForm.vue
+++ src/views/app/CreateComponent/UserForm.vue
@@ -3,60 +3,8 @@
     <b-form @submit.prevent="passes(submit)" autocomplete="off">
       <b-row>
         <b-col v-for="(field,index) in formFields" :key="index" cols="12" :md="cols">
-          <field v-if="field && field.key !== 'address_id'" ref="fields" :disabled="loading || field.disabled || (!create && field.disableOnUpdate)"
+          <field v-if="field && field.key" ref="fields" :disabled="loading || disabled || field.disabled || (!create && field.disableOnUpdate)"
                  :create="create" :inline="inline" :entity="entity" :table-definition="tableDefinition" :field="field"/>
-
-          <!-- adress field -->
-          <field v-if="field && field.key === 'address_id'" ref="fields" :disabled="loading || field.disabled || (!create && field.disableOnUpdate)"
-          :create="create" :inline="inline" :entity="entity" :table-definition="tableDefinition" :field="field">
-            <template #default="{subFormFields, subTableDefinition, subEntity}">
-              <b-row>
-                <b-col cols="12">
-                  <b-form-group :label="$t('attribute.address_street') + '/' + $t('attribute.address_house_number')"
-                    label-cols-md="4">
-                    <div class="d-flex" >
-                      <field style="flex-grow: 1; margin-right: 20px;" :inline="false" :noLabel="true" ref="fields" :disabled="disabled" :entity="subEntity"
-                        :table-definition="subTableDefinition" :field="subFormFields.find(f => f.key === 'address_street')" />
-                      <field style="width: 100px;" :inline="false" :noLabel="true" ref="fields" :disabled="disabled" :entity="subEntity"
-                        :table-definition="subTableDefinition" :field="subFormFields.find(f => f.key === 'address_house_number')" />
-                    </div>
-                  </b-form-group>
-                </b-col>
-                <b-col cols="12">
-                  <field :inline="true" ref="fields" :disabled="disabled" :entity="subEntity" :table-definition="subTableDefinition"
-                    :field="subFormFields.find(f => f.key === 'address_extra')" />
-                </b-col>
-                <b-col cols="12">
-                  <field :inline="true" ref="fields" :disabled="disabled" :entity="subEntity" :table-definition="subTableDefinition"
-                    :field="subFormFields.find(f => f.key === 'city_id')" >
-
-                      <template #default="{subFormFields, subTableDefinition, subEntity}">
-                        <b-row>
-                          <b-col cols="12" >
-                            <b-form-group :label="$t('attribute.city_zip') + '/' + $t('attribute.city_name')"
-                              label-cols-md="4">
-                              <div class="d-flex">
-                                <field style="margin-right: 20px; width: 120px" :inline="false" :noLabel="true" ref="fields" :disabled="disabled" :entity="subEntity" :table-definition="subTableDefinition" :field="subFormFields.find(f => f.key === 'city_zip')"/>
-                                <field style="flex-grow: 1;" :inline="false" :noLabel="true" ref="fields" :disabled="disabled" :entity="subEntity" :table-definition="subTableDefinition" :field="subFormFields.find(f => f.key === 'city_name')"/>
-
-                              </div>
-                            </b-form-group>
-                          </b-col>
-                          <b-col cols="12">
-                            <field :inline="true" ref="fields" :disabled="disabled" :entity="subEntity" :table-definition="subTableDefinition" :field="subFormFields.find(f => f.key === 'state')"/>
-                          </b-col>
-                          <b-col cols="12">
-                            <field :inline="true" ref="fields" :disabled="disabled" :entity="subEntity" :table-definition="subTableDefinition" :field="getCountryField(subFormFields)"/>
-                          </b-col>
-                        </b-row>
-                      </template>
-
-                  </field>
-                </b-col>
-
-              </b-row>
-            </template>
-          </field>
         </b-col>
       </b-row>
     </b-form>
@@ -93,7 +41,6 @@ export default {
           index = definition.fields.findIndex(f => f.key === "partnergroup_id"); // partner group field
           this.$store.dispatch("table/setListData", { entity: definition.fields[index].list, data: data?.partnergroup });
 
-          this.$store.dispatch("table/setListData", { entity: "country", data: data?.country }); // country_id field
         })
         .catch(e => {
           console.error(e);
@@ -101,14 +48,6 @@ export default {
         })
     },
 
-    methods: {
-      getCountryField(formField) {
-        const field = {...formField.find(f => f.key === 'country_id')}
-        field.noFetch = true
-
-        return field
-      },
-    },
 }
 
 </script>
