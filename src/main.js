33ea815e366ea08c3910fa91c47211f940058897
dzoukou <dzoukouraoul@gmail.com>
2023-08-04 11:11:51 +0100

    .

--- src/main.js
+++ src/main.js
@@ -119,7 +119,7 @@ Sentry.init({
     new Sentry.Replay(),
   ],
   // Performance Monitoring
-  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  tracesSampleRate: 0.5, // Capture 100% of the transactions, reduce in production!
   // Session Replay
   replaysSessionSampleRate: env === 'production' ? 0.1 : 1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0,
