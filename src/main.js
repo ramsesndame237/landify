1bb0f02bcb34f55c41a8f9a4b4844fe3c63e4de6
dzoukou <dzoukouraoul@gmail.com>
2023-08-18 11:38:57 +0100

    .

--- src/main.js
+++ src/main.js
@@ -113,15 +113,15 @@ if (process.env.VUE_APP_SENTRY_DNS) {
       return scope
     },
     integrations: [
-      new Sentry.BrowserTracing({
-        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-        tracePropagationTargets: ['localhost', process.env.VUE_APP_BASE_URL],
-        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-      }),
+      // new Sentry.BrowserTracing({
+      //   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      //   tracePropagationTargets: ['localhost', process.env.VUE_APP_BASE_URL],
+      //   routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      // }),
       // new Sentry.Replay(),
     ],
     // Performance Monitoring
-    tracesSampleRate: env === 'production' ? 0.1 : 1, // Capture 100% of the transactions, reduce in production!
+    // tracesSampleRate: env === 'production' ? 0.1 : 1, // Capture 100% of the transactions, reduce in production!
     // Session Replay
     // replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     // replaysOnErrorSampleRate: 1.0,
