7d23aad9a617d59698330b00b737ce0569ae83dd
Ulrich Mbouna <msuslegrand@gmail.com>
2023-09-05 23:22:03 +0100

    feature/Redesign_User_Detail_Page
    - Dans les détails d'un utilisateur, on ne peut modifier les permissions qui lui sont attribuées par son rôle

--- src/views/app/Role/Relation/TableGroupMatrix.vue
+++ src/views/app/Role/Relation/TableGroupMatrix.vue
@@ -12,16 +12,16 @@
         <b-tr v-for="(row,i) in rows" :key="i">
           <b-td>{{ row[labelKey] }}</b-td>
           <b-td>
-            <b-form-checkbox v-model="row.c"/>
+            <b-form-checkbox v-model="row.c" :disabled="isDisabled(row, 'c')"/>
           </b-td>
           <b-td>
-            <b-form-checkbox v-model="row.u"/>
+            <b-form-checkbox v-model="row.u" :disabled="isDisabled(row, 'u')"/>
           </b-td>
           <b-td>
-            <b-form-checkbox v-model="row.r"/>
+            <b-form-checkbox v-model="row.r" :disabled="isDisabled(row, 'r')"/>
           </b-td>
           <b-td>
-            <b-form-checkbox v-model="row.d"/>
+            <b-form-checkbox v-model="row.d" :disabled="isDisabled(row, 'd')"/>
           </b-td>
         </b-tr>
       </b-tbody>
@@ -32,7 +32,7 @@
 
 export default {
   name: 'TableGroupMatrix',
-  props: { relation: Object, entityId: {} },
+  props: { relation: { type: Object, default: () => {} }, entityId: { type: String, required: true }, roleId: { type: Number, default: undefined } },
   data() {
     return {
       loading: false,
@@ -46,30 +46,36 @@ export default {
       return this.table === 'tablegroup' ? 'tablegroup_name' : 'table_name'
     },
     endpoint() {
-      const baseRoute = `/permissions/${this.table === 'tablegroup' ? 'tablegroups' : 'tablenames'}`;
-      let indexRoute,updateRoute = baseRoute
+      const baseRoute = `/permissions/${this.table === 'tablegroup' ? 'tablegroups' : 'tablenames'}`
+      let indexRoute; let updateRoute = baseRoute
 
       if (this.relation.entityName && this.relation.entityName !== 'role') {
-        indexRoute = `${baseRoute}/${this.relation.entityName}/${this.entityId}`;
-        updateRoute = `${baseRoute}/${this.relation.entityName}`;
+        indexRoute = `${baseRoute}/${this.relation.entityName}/${this.entityId}`
+        updateRoute = `${baseRoute}/${this.relation.entityName}`
       } else {
-        indexRoute = `${baseRoute}/${this.entityId}`;
-        updateRoute = `${baseRoute}/${this.entityId}`;
+        indexRoute = `${baseRoute}/${this.entityId}`
+        updateRoute = `${baseRoute}/${this.entityId}`
       }
 
       return {
         baseRoute,
         indexRoute,
-        updateRoute
+        updateRoute,
       }
     },
     rows() {
-      return this.$store.getters['table/listCache'](`table-group-${this.entityId}`)
-    }
+      return this.$store.getters['table/listCache'](`table-group-${this.relation.entityName}-${this.entityId}`)
+    },
+    userRolePermissions() {
+      return this.$store.getters['table/listCache'](`table-group-${this.roleId}`)
+    },
   },
   async mounted() {
-    if(this.rows.length <= 0) {
+    if (this.rows.length <= 0) {
       await this.loadData()
+    }
+    if (this.relation.entityName === 'user') {
+      await this.getUserRolePermissions()
     }
   },
   beforeDestroy() {
@@ -84,31 +90,57 @@ export default {
       this.loading = true
       try {
         await this.$http.put(this.endpoint.updateRoute, payload)
-        this.$successToast('Table Group assigned successfully')
+        this.$successToast('Table Group updated successfully')
       } catch (err) {
         console.error('err: ', err)
       } finally {
         this.loading = false
       }
     },
-    async loadData() {
+    async loadData(customLoad = false) {
       this.loading = true
-      await this.$http.get(this.endpoint.indexRoute)
+      let endpoint
+      if (!customLoad) {
+        endpoint = this.endpoint.indexRoute
+      } else {
+        endpoint = `${this.endpoint.baseRoute}/${this.roleId}`
+      }
+      await this.$http.get(endpoint)
         .then(resp => {
           const { data } = resp
-          this.$store.dispatch('table/setListData', { entity: `table-group-${this.entityId}`, data: data })
-
+          if (!customLoad) {
+            this.$store.dispatch('table/setListData', { entity: `table-group-${this.relation.entityName}-${this.entityId}`, data })
+            this.$root.$on('update-matrix', this.submit)
+          } else {
+            this.$store.dispatch('table/setListData', { entity: `table-group-${this.roleId}`, data })
+          }
           this.loading = false
-          this.$root.$on('update-matrix', this.submit)
         })
         .finally(() => {
           this.loading = false
         })
-    }
+    },
+    async getUserRolePermissions() {
+      if (this.userRolePermissions.length <= 0) {
+        try {
+          // Je récupère les permissions liées au rôle de l'utilisateur
+          await this.loadData(true)
+        } catch (error) {
+          console.log({ error })
+        }
+      }
+    },
+    isDisabled(row, operation) {
+      const tablegroup = this.userRolePermissions.find(tgroup => row.tablegroup_id === tgroup.tablegroup_id)
+      if (tablegroup) {
+        return tablegroup[operation]
+      }
+
+      return false
+    },
   },
 }
 </script>
-
 <style scoped>
 
 </style>
