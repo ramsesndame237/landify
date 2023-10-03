e1cf5477f580154e1c4e2ad752c8d1cd5cec5beb
Ulrich Mbouna <msuslegrand@gmail.com>
2023-10-03 10:29:53 +0100

    feature/Dashboard
    - Lorsqu'un utilisateur est administrateur, on devrait mettre ses companies à All dans les filtres des tickets clients

--- src/views/app/DashboardAnalytic.vue
+++ src/views/app/DashboardAnalytic.vue
@@ -143,7 +143,7 @@ export default {
   mounted() {
     this.resetDatePicker()
     setTimeout(() => {
-      if (this.initData && this.entity?.company_id) {
+      if (Object.keys(this.initData).length > 0 && this.initData.company_id !== undefined && this.entity?.company_id) {
         this.entity.company_id = this.initData?.company_id
         if (this.isTeamExistInList(this.initData?.team_id)) {
           this.entity.team_id = this.initData?.team_id
