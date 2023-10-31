77cb19aa174e1dd6e4fdb4d3b786ffc12da2296d
ramsesndame237 <ramsesndame34@gmail.com>
2023-10-31 12:27:02 +0100

    Feat:change companie form refs : #5

---
+++ src/views/app/CustomComponents/BreadCrumb/BreadCrumb.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="breadcrumb-wrapper">
+    <b-breadcrumb class="px-0">
+      <b-breadcrumb-item to="/">
+        <feather-icon icon="HomeIcon" size="16" class="align-text-top"/>
+      </b-breadcrumb-item>
+      <b-breadcrumb-item
+        v-for="(item, index) in $route.meta.breadcrumb"
+        :key="`route-meta-${item.text}-${index}`"
+        :active="item.active"
+        :to="item.to"
+      >
+        <span class="text-capitalize">
+          {{ $t(item.text) }}
+        </span>
+      </b-breadcrumb-item>
+      <b-breadcrumb-item
+        v-for="(item, index) in data"
+        :key="`data-meta-${item.text}-${index}`"
+        :active="item.active"
+        :to="item.to"
+      >
+        <span class="text-capitalize">
+          {{ $t(item.text) }}
+        </span>
+      </b-breadcrumb-item>
+    </b-breadcrumb>
+  </div>
+</template>
+
+<script>
+import { BBreadcrumb, BBreadcrumbItem } from 'bootstrap-vue'
+
+export default {
+  components: {
+    BBreadcrumbItem,
+    BBreadcrumb,
+  },
+  props: {
+    data: {
+      type: Array,
+      default: () => [],
+    },
+    noRouteMeta: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
