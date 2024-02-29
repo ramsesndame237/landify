394b8bbde6ade0756fd73b9c95410a661d43a228
ramsesndame237 <ramsesndame34@gmail.com>
2024-02-29 13:30:24 +0100

    fix:nofication fonctionnality and the ticket list update view

--- src/@core/layouts/components/app-navbar/components/NotificationDropdown.vue
+++ src/@core/layouts/components/app-navbar/components/NotificationDropdown.vue
@@ -1,101 +1,107 @@
 <template>
-  <b-nav-item-dropdown
-    class="dropdown-notification mr-25 "
-    menu-class="dropdown-menu-media"
-    right
-  >
-    <template #button-content>
-      <feather-icon
-        :badge="newNotification"
-        badge-classes="bg-danger"
-        class="text-body"
-        icon="BellIcon"
-        size="21"
-      />
-    </template>
-
-    <!-- Header -->
-    <li class="dropdown-menu-header" v-if="newNotification > 0">
-      <div class="dropdown-header d-flex">
-        <h4 class="notification-title mb-0 mr-auto">
-          Notifications
-        </h4>
-        <b-badge
-          pill
-          variant="light-primary"
+        <b-nav-item-dropdown
+          class="dropdown-notification mr-25 "
+          menu-class="dropdown-menu-media"
+          ref="notificationNav"
+          right
         >
-          {{ newNotification }} New
-        </b-badge>
-      </div>
-    </li>
-    <b-spinner label="Spinning" class="m-2 text-primary" size="lg" v-if="loadingNotificaton"></b-spinner>
-    <div class="scrollable-container media-list scroll-area overflow-y-scroll overflow-x-scroll"
-         v-if="!loadingNotificaton">
-      <div
-        v-for="(notification,index) in notifications"
-        :key="index"
-        class="relative cursor-pointer"
-        @click="$router.push(`/app/table/ticket/view/${notification.payload_json.ticket_id}`)"
-        @mouseenter="notificationAction = notification.id"
-      >
-        <div v-if="notification.id === notificationAction"
-             class="position-absolute position-right-0 position-top-2 d-flex ">
-          <b-avatar v-if="notification.read === 0" v-b-tooltip.hover :title="$t('translate~key~mark~read')"
-                    :class="['mx-2',notification.read === 0 ? 'bg-primary':'']"
-                    size="sm"
-                    @click.native="markedNotificationAsRead(notification.id)">
-            <feather-icon size="10" :icon="notification.read === 0 ?  'MailIcon' : 'BookOpenIcon'"
-                          class="position-absolute"/>
-          </b-avatar>
-          <b-avatar v-b-tooltip.hover title="Delete notification"
-                    :class="[notification.read === 0 ? 'bg-primary':'']"
-                    size="sm"
-                    @click.native="deleteNotification(notification.id)">
-            <feather-icon size="10" icon="TrashIcon"
-                          class="position-absolute"/>
-          </b-avatar>
+          <template #button-content>
+            <feather-icon
+              :badge="newNotification"
+              badge-classes="bg-danger"
+              class="text-body"
+              icon="BellIcon"
+              size="21"
+            />
+          </template>
 
-        </div>
-        <b-media>
-          <p class="media-heading text-ellipsis overflow-hidden" style="max-height: 80px">
+          <!-- Header -->
+          <li class="dropdown-menu-header" v-if="newNotification > 0">
+            <div class="dropdown-header d-flex">
+              <h4 class="notification-title mb-0 mr-auto">
+                Notifications
+              </h4>
+              <b-badge
+                pill
+                variant="light-primary"
+              >
+                {{ newNotification }} New
+              </b-badge>
+            </div>
+          </li>
+          <b-spinner label="Spinning" class="m-2 text-primary" size="lg" v-if="loadingNotificaton"></b-spinner>
+          <div class="scrollable-container media-list scroll-area overflow-y-scroll overflow-x-scroll"
+               v-if="!loadingNotificaton">
+            <div
+              v-for="(notification,index) in notifications"
+              :key="index"
+              class="relative cursor-pointer"
+              @click="$router.push(`/app/table/ticket/view/${notification.payload_json.ticket_id}`)"
+              @mouseenter="notificationAction = notification.id"
+            >
+              <div v-if="notification.id === notificationAction"
+                   class="position-absolute position-right-0 position-top-2 d-flex ">
+                <b-avatar v-if="notification.read === 0" v-b-tooltip.hover :title="$t('translate~key~mark~read')"
+                          :class="['mx-2',notification.read === 0 ? 'bg-primary':'']"
+                          size="sm"
+                          @click.native="markedNotificationAsRead(notification.id)">
+                  <feather-icon size="10" :icon="notification.read === 0 ?  'MailIcon' : 'BookOpenIcon'"
+                                class="position-absolute"/>
+                </b-avatar>
+                <b-avatar v-b-tooltip.hover title="Delete notification"
+                          :class="[notification.read === 0 ? 'bg-primary':'']"
+                          size="sm"
+                          @click.native="deleteNotification(notification.id)">
+                  <feather-icon size="10" icon="TrashIcon"
+                                class="position-absolute"/>
+                </b-avatar>
+
+              </div>
+              <b-media>
+                <p class="media-heading text-ellipsis overflow-hidden d-flex justify-content-between"
+                   style="max-height: 80px">
             <span class="font-weight-bolder">
               {{
                 current_lang == 'en' ? notification.payload_json.title.en : current_lang === 'de' ? notification.payload_json.title.de : current_lang === 'fr' ? notification.payload_json.title.fr : notification.payload_json.title.en
               }}
             </span>
-          </p>
-          <small class="notification-text text-ellipsis overflow-hidden  " style="max-height:100px;">{{
-              urrent_lang == 'en' ? notification.payload_json.content.en : current_lang === 'de' ? notification.payload_json.content.de : current_lang === 'fr' ? notification.payload_json.content.fr : notification.payload_json.content.en
-            }}</small>
-        </b-media>
-      </div>
-    </div>
+                  <small>
+                    {{ formatDate(notification.timespan) }}
+                  </small>
+                </p>
+                <small class="notification-text text-ellipsis overflow-hidden  " style="max-height:100px;">{{
+                    urrent_lang == 'en' ? notification.payload_json.content.en : current_lang === 'de' ? notification.payload_json.content.de : current_lang === 'fr' ? notification.payload_json.content.fr : notification.payload_json.content.en
+                  }}</small>
+              </b-media>
+            </div>
+          </div>
 
-    <!-- Cart Footer -->
-    <li class="dropdown-menu-footer d-flex overflow-hidden">
-      <b-button
-        class="mx-1"
-        v-ripple.400="'rgba(255, 255, 255, 0.15)'"
-        variant="warning"
+          <!-- Cart Footer -->
+          <li class="dropdown-menu-footer d-flex overflow-hidden">
+            <b-button
+              class="mx-1"
+              v-ripple.400="'rgba(255, 255, 255, 0.15)'"
+              variant="warning"
 
-        @click="() => markedNotificationAsRead(undefined) "
-      >{{ $t('translate~key~read~all~notification') }}
-      </b-button>
-      <b-button
-        v-ripple.400="'rgba(255, 255, 255, 0.15)'"
-        variant="primary"
+              @click="() => markedNotificationAsRead(undefined) "
+            >{{ $t('translate~key~read~all~notification') }}
+            </b-button>
+            <b-button
+              v-ripple.400="'rgba(255, 255, 255, 0.15)'"
+              variant="primary"
 
-        @click="() => deleteNotification(undefined) "
-      >${{$t('translate~key~delete~all~notification')}}
-      </b-button>
-    </li>
-  </b-nav-item-dropdown>
+              @click="() => deleteNotification(undefined) "
+            >${{ $t('translate~key~delete~all~notification') }}
+            </b-button>
+          </li>
+        </b-nav-item-dropdown>
 </template>
 
 <script>
-import {BAvatar, BBadge, BButton, BFormCheckbox, BLink, BMedia, BNavItemDropdown,} from 'bootstrap-vue'
+import {BAvatar, BBadge, BButton, BFormCheckbox, BLink, BMedia, BNavbar, BNavItemDropdown,} from 'bootstrap-vue'
 import VuePerfectScrollbar from 'vue-perfect-scrollbar'
 import Ripple from 'vue-ripple-directive'
+import {formatDate} from "../../../../../views/app/CustomComponents/DataTable/utils";
 
 export default {
   components: {
@@ -104,6 +110,7 @@ export default {
     BMedia,
     BLink,
     BAvatar,
+    BNavbar,
     VuePerfectScrollbar,
     BButton,
     BFormCheckbox,
@@ -135,6 +142,7 @@ export default {
     this.clearInterval()
   },
   methods: {
+    formatDate,
     getNumberUnreadedNotification() {
       this.$http.get('/notifications/unread/count').then((response) => {
         this.newNotification = response.data
