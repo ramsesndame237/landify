af7b177143f254d6e096ffd3d534efc8ec7e3d14
__M__ <ndazoomessanga@gmail.com>
2024-03-01 18:56:47 +0100

    fix: issue with jwt

--- src/@core/auth/jwt/jwtService.js
+++ src/@core/auth/jwt/jwtService.js
@@ -47,10 +47,6 @@ export default class JwtService {
     this.axiosIns.interceptors.response.use(
       response => response,
       error => {
-        setInterval(() => {
-          checkAndverficationJwt()
-        }, 10 * 60 * 1000)
-
         // const { config, response: { status } } = error
         setInterval(() => {
           checkAndverficationJwt()
