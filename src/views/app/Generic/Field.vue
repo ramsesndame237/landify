6edf414c2024bcee4cae22bf6aeed851f8597747
__m__ <ndazoomessanga@gmail.com>
2024-03-13 17:44:08 +0100

    fix: extern visibility

--- src/views/app/Generic/Field.vue
+++ src/views/app/Generic/Field.vue
@@ -573,7 +573,7 @@ export default {
     },
     initializeValue() {
       const user = getUserData()
-      if (this.isUserExternClient || this.$isUserExtern) {
+      if (this.isUserExternClient || user?.usertype?.usertype_external === 1) {
         if (this.field.key === 'customergroup_id') {
           console.log('reset ', this.entity)
           const customergroup_id = user.customergroup?.customergroup_id
